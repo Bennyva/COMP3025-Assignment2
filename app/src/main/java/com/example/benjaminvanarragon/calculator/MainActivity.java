99918f732362bddce10d5cc5fe8d9bb0245061d9
bennyva <benjaminvanarragon@gmail.com>
2016-03-10 13:35:09 -0500

    added code behind again

--- app/src/main/java/com/example/benjaminvanarragon/calculator/MainActivity.java
+++ app/src/main/java/com/example/benjaminvanarragon/calculator/MainActivity.java
@@ -11,71 +11,134 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    //variables for current number, results, current operation, and labels
+    TextView textView = (TextView) findViewById(R.id.textView);
+    float result = 0;
+    float currentNum = 0;
+    String currentNumString = "";
+    String currentOp = "";
+    String labelString = "";
+    float storedNum = 0;
+    String currentOperation = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        currentOp = "=";
+        textView.setText(String.valueOf(result));
     }
 
-    ArrayList<String> arrayList = new ArrayList<String>();
-    String string = "";
-    String string1 = "";
-    public void onClick1(View v){
-        TextView textView = (TextView) findViewById(R.id.textView);
-        Button button = (Button) v;
+    //This method fires when a number is pressed, it sets the current number to whatever the user is sending through this method. Then procedes to set the label to the current number.
+    public void btnNumberInput(View v) {
+        //sets the current number to what is sent
+        //currentNum = currentNum * 10 + Float(sender.currentTitle!)!
 
-        string = (String) button.getText().toString();
+        labelString = textView.getText().toString();
 
-        if(!string.contains("+") && !string.contains("-") && !string.contains("*") && !string.contains("/")){
-            string1 = string1 + string;
 
-            if(arrayList.size()>0){
-                arrayList.remove(arrayList.size()-1);
-
-            }
-            arrayList.add(string1);
-
+        if(labelString == "0"){
+            currentNumString = v.toString();
         }
         else{
-            arrayList.add(string);
-            arrayList.add(string);
-            string1="";
+            currentNumString = currentNumString + v.toString();
         }
-
-        //textView.setText(textView.getText().toString()+string);
-
-        textView.setText(arrayList.toString());
+        currentNum = Float.parseFloat(currentNumString);
+        System.out.println("\ncurrent num: " + currentNum);
+        System.out.println("\nStored Num: " + result);
+        System.out.println("\nResult: " + result);
+        //sets the result label to the current number
+        textView.setText(String.valueOf(currentNum));
     }
 
-    public void onClick(View v){
-        TextView textView = (TextView)findViewById(R.id.textView);
 
-        int calc = 0;
-        int c = arrayList.size();
+    //This method fires when an operation button is pressed. Each case will fire when the appropriate sign is clicked. Then sets the result label to the result.
+    public void btnOperation(View v) {
 
-        while(c!=1){
+        //sets the current Operation to what is clicked
+        currentOp = v.toString();
+        switch (currentOp){
+            //all the math is done here
+            case "=":
+                //if statements to check what the current operation was for the equation
+                if(currentOperation == "+"){
+                    //sets the result to num1 + num2
+                    result = storedNum + currentNum;
+                    //sets the currentNum to the result in case the user wants to append math functions on the equation
+                    currentNum = result;
+                }
+                else if(currentOperation == "-"){
+                    result = storedNum - currentNum;
+                    currentNum = result;
+                }
+                else if(currentOperation == "*"){
+                    result = storedNum * currentNum;
+                    currentNum = result;
+                }
+                else if(currentOperation == "/"){
+                    result = storedNum / currentNum;
+                    currentNum = result;
+                }else{
+                    System.out.println("error");
+                    textView.setText("err");
+                }
 
-            if(arrayList.get(1).contains("+")){calc = Integer.parseInt(arrayList.get(0))+Integer.parseInt(arrayList.get(2));}
-            if(arrayList.get(1).contains("-")){calc = Integer.parseInt(arrayList.get(0))-Integer.parseInt(arrayList.get(2));}
-            if(arrayList.get(1).contains("*")){calc = Integer.parseInt(arrayList.get(0))*Integer.parseInt(arrayList.get(2));}
-            if(arrayList.get(1).contains("/")){calc = Integer.parseInt(arrayList.get(0))/Integer.parseInt(arrayList.get(2));}
+            case "+":
+                //sets the current operation for the math function
+                currentOperation = "+";
+                //sets a stored num, so the second num memery space is clear
+                storedNum = currentNum;
+                //rests the currentNum so a new value can be placed in the memory space
+                currentNumString = "0";
+                currentNum = 0;
 
-            textView.setText(Integer.toString(calc));
+            case "-":
+                currentOperation = "-";
+                storedNum = currentNum;
+                currentNumString = "0";
+                currentNum = 0;
+
+            case "*":
+                currentOperation = "*";
+                storedNum = currentNum;
+                currentNumString = "0";
+                currentNum = 0;
+
+            case "/":
+                currentOperation = "/";
+                storedNum = currentNum;
+                currentNumString = "0";
+                currentNum = 0;
 
 
-
+                //default switch case if none are available.
+            default:
+                System.out.println("error");
         }
+
+        //currentNumString = "0"
+        //currentNum = 0
+        textView.setText(String.valueOf(result));
+
+        //if(sender.titleLabel!.text == "="){
+        //    result = 0
+        //}
+        //sets the current operation to what is sent through the method
+        currentOp = v.toString();
     }
 
-    public void clear(View v){
-        TextView textView = (TextView)findViewById(R.id.textView);
 
-            string1 = "";
-            string = "";
-            textView.setText("0");
-            arrayList.clear();
-        }
+    //resets all variables to initial values, and clears the label
+    public void btnClear(View v) {
+        result = 0;
+        currentNum = 0;
+        currentOp = "=";
+        textView.setText(String.valueOf(result));
+        currentNumString = "0";
+
     }
+}
+
 
 
 
